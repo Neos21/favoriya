3b32ffa2584e3494029b33f3a4bee84c1e2488e6
Neos21 <neos21@gmail.com>
2024-12-01 22:00:06 +0900

    Update ESLint Rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -13,11 +13,11 @@ export default tseslint.config(
     ]
   },
   {
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended
     ],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser
@@ -30,7 +30,9 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       
-      quotes: ['error', 'single']
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'never']
     }
   },
   
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -4,5 +4,5 @@ export const Home = () => {
   return <>
     <h1>Pseudo</h1>
     <p><Button variant="contained">Button</Button></p>
-  </>
+  </>;
 };
