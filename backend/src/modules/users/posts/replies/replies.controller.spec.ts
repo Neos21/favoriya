7a56d7c4c1d7b90bd186306127e233482e8e6d35
Neos21 <neos21@gmail.com>
2024-12-23 08:04:14 +0900

    Fix

--- backend/src/modules/users/posts/replies/replies.controller.spec.ts
+++ backend/src/modules/users/posts/replies/replies.controller.spec.ts
@@ -1,19 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { JwtAuthGuard } from '../../../../shared/guards/jwt-auth.guard';
+import { PostDecorationService } from '../post-decoration.service';
+import { PostValidationService } from '../post-validation.service';
 import { RepliesController } from './replies.controller';
 import { RepliesService } from './replies.service';
 
 describe('RepliesController', () => {
   let controller: RepliesController;
   let fakeRepliesService: Partial<RepliesService>;
+  let fakePostValidationService: Partial<PostValidationService>;
+  let fakePostDecorationService: Partial<PostDecorationService>;
   
   beforeEach(async () => {
     const fakeJwtAuthGuard = jest.fn(() => true);
     
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RepliesController],
-      providers: [{ provide: RepliesService, useValue: fakeRepliesService }]
+      providers: [
+        { provide: RepliesService, useValue: fakeRepliesService },
+        { provide: PostValidationService, useValue: fakePostValidationService },
+        { provide: PostDecorationService, useValue: fakePostDecorationService }
+      ]
     })
       .overrideGuard(JwtAuthGuard)
       .useValue(fakeJwtAuthGuard)
--- backend/src/modules/users/posts/replies/replies.controller.ts
+++ backend/src/modules/users/posts/replies/replies.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Get, HttpStatus, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
 
+import { topicsConstants } from '../../../../common/constants/topics-constants';
 import { camelToSnakeCaseObject, snakeToCamelCaseObject } from '../../../../common/helpers/convert-case';
 import { JwtAuthGuard } from '../../../../shared/guards/jwt-auth.guard';
 import { isValidJwtUserId } from '../../../../shared/helpers/is-valid-jwt-user-id';
+import { PostDecorationService } from '../post-decoration.service';
+import { PostValidationService } from '../post-validation.service';
 import { RepliesService } from './replies.service';
 
 import type { Request, Response } from 'express';
@@ -11,9 +14,13 @@ import type { Post as TypePost, PostApi } from '../../../../common/types/post';
 /** Replies Controller */
 @Controller('api/users')
 export class RepliesController {
-  constructor(private readonly repliesService: RepliesService) { }
+  constructor(
+    private readonly repliesService: RepliesService,
+    private readonly postValidationService: PostValidationService,
+    private readonly postDecorationService: PostDecorationService,
+  ) { }
   
-  /** リプライする */
+  /** リプライ一覧を取得する */
   @UseGuards(JwtAuthGuard)
   @Get(':userId/posts/:postId/replies')
   public async findAllAfterReplies(@Param('userId') inReplyToUserId: string, @Param('postId') inReplyToPostId: string, @Res() response: Response): Promise<Response<Array<PostApi>>> {
@@ -31,6 +38,16 @@ export class RepliesController {
     if(!isValidJwtUserId(request, response, postApi.user_id)) return;  // リプライする人の本人確認
     
     const post: TypePost = snakeToCamelCaseObject(postApi) as TypePost;
+    
+    // トピックごとに入力チェックする
+    const validationResult = this.postValidationService.validateText(post.text, post.topicId);
+    if(validationResult.error != null) return response.status(validationResult.code ?? HttpStatus.BAD_REQUEST).json(validationResult);
+    
+    // 川柳モードの時にスタイリングできそうならする
+    if(post.topicId === topicsConstants.senryu.id) post.text = this.postDecorationService.senryuStyle(post.text);
+    // ランダム装飾モードの場合に行ごとにタグを入れたり入れなかったりする
+    if(post.topicId === topicsConstants.randomDecorations.id) post.text = this.postDecorationService.decorateRandomly(post.text);
+    
     const result = await this.repliesService.create(inReplyToUserId, inReplyToPostId, post);
     if(result.error != null) return response.status(result.code ?? HttpStatus.BAD_REQUEST).json(result);
     
