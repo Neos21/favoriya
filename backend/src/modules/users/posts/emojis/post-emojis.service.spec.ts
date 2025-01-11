7eddd7a0e04060cdd692f4446bc6bfddd0a2bb9f
Neos21 <neos21@gmail.com>
2025-01-11 19:59:36 +0900

    Fix

--- backend/src/modules/users/posts/emojis/post-emojis.service.spec.ts
+++ backend/src/modules/users/posts/emojis/post-emojis.service.spec.ts
@@ -4,17 +4,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
 import { EmojiReactionEntity } from '../../../../shared/entities/emoji-reaction.entity';
+import { NotificationsService } from '../../../notifications/notifications.service';
 import { PostEmojisService } from './post-emojis.service';
 
 describe('PostEmojisService', () => {
   let service: PostEmojisService;
   let fakePostEmojisRepository: Partial<Repository<EmojiReactionEntity>>;
+  let fakeNotificationsService: Partial<NotificationsService>;
   
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PostEmojisService,
-        { provide: getRepositoryToken(EmojiReactionEntity), useValue: fakePostEmojisRepository }
+        { provide: getRepositoryToken(EmojiReactionEntity), useValue: fakePostEmojisRepository },
+        { provide: NotificationsService, useValue: fakeNotificationsService }
       ]
     }).compile();
     
