f2527a7f1961e5fea187fc6ababbbf6fb3887bde
Neos21 <neos21@gmail.com>
2025-02-05 05:18:49 +0900

    Update

--- frontend/src/core/components/ErrorFallbackComponent/ErrorFallbackComponent.tsx
+++ frontend/src/core/components/ErrorFallbackComponent/ErrorFallbackComponent.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Divider, Typography } from '@mui/material';
 
 /** Error Fallback Component */
 export const ErrorFallbackComponent: FC = () => {
@@ -8,6 +8,11 @@ export const ErrorFallbackComponent: FC = () => {
     <Box component="div">
       <Typography component="h1">予期しないエラーが発生しました・ゴメンね</Typography>
       <Button variant="contained" sx={{ mt: 3 }} onClick={() => { location.href = '/'; }}>トップに戻る</Button>
+      <Divider sx={{ mt: 3 }} />
+      <Button variant="contained" sx={{ mt: 3 }} onClick={() => { window.open('https://legacy-of-bbs.pages.dev'); }}>
+        エラーが続いたらコチラにドウゾ<br />
+        <strong>懐かし掲示板 BBS</strong>
+      </Button>
     </Box>
   </Box>;
 };
